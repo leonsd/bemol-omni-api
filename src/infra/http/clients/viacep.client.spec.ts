283285554afe5a897a15da548d093fdd1f326ca9
Leonardo S <leon.sdsilva@gmail.com>
2023-08-19 15:16:56 -0300

    test: ensure ViaCepClient return correct address on success

--- src/infra/http/clients/viacep.client.spec.ts
+++ src/infra/http/clients/viacep.client.spec.ts
@@ -1,10 +1,36 @@
 import { ViaCepClient } from './viacep.client';
 import { HttpClient } from '../../../data/protocols/http/client';
 
+const makeFakeViaCepAddress = () => {
+  return {
+    cep: '01001-000',
+    logradouro: 'Praça da Sé',
+    complemento: 'lado ímpar',
+    bairro: 'Sé',
+    localidade: 'São Paulo',
+    uf: 'SP',
+    ibge: '3550308',
+    gia: '1004',
+    ddd: '11',
+    siafi: '7107',
+  };
+};
+
+const makeFakeAddressSearched = () => {
+  return {
+    zipCode: '01001-000',
+    street: 'Praça da Sé',
+    complement: 'lado ímpar',
+    neighborhood: 'Sé',
+    city: 'São Paulo',
+    state: 'SP',
+  };
+};
+
 const makeAxiosAdapter = () => {
   class AxiosAdapterStub implements HttpClient {
     async get<T>(uri: string): Promise<T | null> {
-      return 'any_return' as T;
+      return makeFakeViaCepAddress() as T;
     }
   }
 
@@ -34,5 +60,13 @@ describe('ViaCep Client', () => {
 
     await sut.findAddressByZipCode(zipCode);
     expect(getSpy).toHaveBeenCalledWith(`viacep.com.br/ws/${zipCode}/json/`);
+  });
+
+  test('Should return correct address on success', async () => {
+    const { sut } = makeSut();
+    const zipCode = 'any_zip_code';
+
+    const response = await sut.findAddressByZipCode(zipCode);
+    expect(response).toEqual(makeFakeAddressSearched());
   });
 });
